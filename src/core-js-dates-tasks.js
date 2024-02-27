2318df2ce645f7eadfd59d078c226fad86e8f788
Yuliya-Karuk <karuk.yuliya@gmail.com>
2024-02-28 01:20:29 +0400

    feat: add solutions of 2 tasks

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -263,8 +263,29 @@ function getQuarter(date) {
  * { start: '01-01-2024', end: '15-01-2024' }, 1, 3 => ['01-01-2024', '05-01-2024', '09-01-2024', '13-01-2024']
  * { start: '01-01-2024', end: '10-01-2024' }, 1, 1 => ['01-01-2024', '03-01-2024', '05-01-2024', '07-01-2024', '09-01-2024']
  */
-function getWorkSchedule(/* period, countWorkDays, countOffDays */) {
-  throw new Error('Not implemented');
+function getWorkSchedule(period, countWorkDays, countOffDays) {
+  const startPeriod = new Date(period.start.split('-').reverse().join('-'));
+  const endPeriod = new Date(period.end.split('-').reverse().join('-'));
+  let schedule = [];
+  let isWorked = true;
+  let day = startPeriod;
+  while (day <= endPeriod) {
+    if (isWorked) {
+      for (let i = 0; i < countWorkDays && day <= endPeriod; i += 1) {
+        schedule.push(day.toJSON());
+        day = new Date(day.setDate(day.getDate() + 1));
+      }
+      isWorked = false;
+    }
+    if (!isWorked) {
+      day = new Date(day.setDate(day.getDate() + countOffDays));
+      isWorked = true;
+    }
+  }
+  schedule = schedule.map((el) =>
+    el.slice(0, 10).split('-').reverse().join('-')
+  );
+  return schedule;
 }
 
 /**
@@ -279,8 +300,12 @@ function getWorkSchedule(/* period, countWorkDays, countOffDays */) {
  * Date(2022, 2, 1) => false
  * Date(2020, 2, 1) => true
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  const year = date.getFullYear();
+  if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+    return true;
+  }
+  return false;
 }
 
 module.exports = {
